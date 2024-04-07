b356b0926ca9e094217b2de3202772978f87e3c1
ArtemMor <moruginartem99@gamil.com>
2024-04-08 01:21:31 +0300

    added endpoint to create chat + fixes

---
+++ app/tests/fixtures.py
@@ -0,0 +1,14 @@
+from punq import Container, Scope
+
+# from infra.repositories.messages.base import BaseChatRepository
+# from infra.repositories.messages.memory import MemoryChatRepository
+from logic.init import _init_container
+
+from infra.repositories.messages import BaseChatRepository, MemoryChatRepository
+
+
+def init_dummy_container() -> Container:
+    container = _init_container()
+    container.register(BaseChatRepository, MemoryChatRepository, scope=Scope.singleton)
+
+    return container
